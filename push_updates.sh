0ac5627d6622e205ac7cdcdfb7f7fc1d203fe1a9
Stefano Terna <stefano.terna@tomorrowdata.io>
2020-12-19 14:47:56 +0100

    push daily updates

---
+++ push_updates.sh
@@ -0,0 +1,11 @@
+git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.csv
+git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.xlsx
+git add images/TD_Rt_computation_MCMC_300dpi.png
+git add images/TD_Rt_computation_MCMC_300dpi.jpg
+git add notebooks/Rt_on_italian_national_data.ipynb
+
+git commit -m "update $(date --iso-8601)"
+#git commit -m "update YYYY-mm-dd"
+
+
+git push
