7d1f29d73649efbd2034d7f12cadd04a2f94cdec
Stefano Terna <stefano.terna@tomorrowdata.io>
2021-02-28 11:33:28 +0100

    add truncnorm with standard interface

--- covid19_pytoolbox/utils.py
+++ covid19_pytoolbox/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from scipy import stats
 
 def padnan(a, paddings, value=np.nan):
     return np.pad(a, paddings, mode='constant', constant_values=(value, value))
@@ -11,3 +12,7 @@ def smape(A, F):
         F = F.to_numpy()
         
     return 1/len(A) * np.sum(2 * np.abs(F - A) / (np.abs(A) + np.abs(F)))
+
+def truncnorm(mu, sigma, lower, upper, size):
+    tn = stats.truncnorm((lower - mu) / sigma, (upper - mu) / sigma, loc=mu, scale=sigma)
+    return tn.rvs(size)
