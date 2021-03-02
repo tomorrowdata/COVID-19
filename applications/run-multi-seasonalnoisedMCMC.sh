e5858c985755bb761be60ffc068150ae895017b9
Stefano Terna <stefano.terna@tomorrowdata.io>
2021-03-02 13:07:24 +0100

    allow running multiple applications

--- applications/run-multi-seasonalnoisedMCMC.sh
+++ applications/run-multi-seasonalnoisedMCMC.sh
@@ -1,14 +1,16 @@
 #docker build -f Dockerfile-app -t covid-run-apps .
 
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 119 110
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 109 100
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 99 90
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 89 80
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 79 70
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 69 60
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 59 50
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 49 40
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 39 30
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 29 20
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 19 10
-docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 seasonalnoisedMCMC.py 9 0
+APP=$1
+
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 119 110
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 109 100
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 99 90
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 89 80
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 79 70
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 69 60
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 59 50
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 49 40
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 39 30
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 29 20
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 19 10
+docker run -d -v `pwd`/../data:/home/jupuser/data covid-run-apps python3 $APP 9 0
