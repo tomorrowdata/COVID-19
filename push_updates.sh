9aacf8e9a200a2409d5ab9818e4340f2bbda0e59
Stefano Terna <stefano.terna@tomorrowdata.io>
2021-03-16 11:58:32 +0100

    avoid committing ISS data daily

--- push_updates.sh
+++ push_updates.sh
@@ -1,5 +1,5 @@
-#UDATE=$(date --iso-8601)
-UDATE="2021-03-12"
+UDATE=$(date --iso-8601)
+#UDATE="YYYY-MM-DD"
 
 
 git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.csv
@@ -7,7 +7,6 @@ git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.xlsx
 git add images/TD_Rt_computation_MCMC_300dpi.png
 git add images/TD_Rt_computation_MCMC_300dpi.jpg
 git add notebooks/Rt_on_italian_national_data.ipynb
-git add data/sources/Rt_Piemonte_from_ISS.csv
 git add notebooks/italy/Rt_Piemonte.ipynb
 
 git add "data/computed/italy/${UDATE}_italy_futbound-08-12_draws-5_MCMC_Rt_pastdays_000_000.pickle"
