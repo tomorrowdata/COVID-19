1aa7fd98ce619c6a9ec00f9bb7f01be61eb1aa32
Stefano Terna <stefano.terna@tomorrowdata.io>
2021-03-22 16:05:12 +0100

    parametrize max_chain_len and targetaccept

--- applications/seasonalnoisedMCMC_regional.py
+++ applications/seasonalnoisedMCMC_regional.py
@@ -32,16 +32,16 @@ def save_MCMC_sampling(df, column, trace, pastdays, interval=0.95, start=0):
         sampling_hdi[:,1], (start,pastdays))
 
 
-def compute_past_series(df, new_cases_col, pastdays_start, pastdays_end, draws, alpha, beta, trend_alpha, lower_ratio, upper_ratio, pickleprefix, 
+def compute_past_series(df, new_cases_col, startday, pastdays_start, pastdays_end, draws, alpha, beta, trend_alpha, lower_ratio, upper_ratio, pickleprefix, 
     mctune=1000, mcdraws=500, mccores=8, mctargetaccept=0.95, rt_col_prefix='smooth_deseas'):
 
     for pastdays in range(pastdays_start, pastdays_end-1,-1):
         print(f'\npastdays: {pastdays}')
 
         if pastdays == 0:
-            sl = np.s_[:]
+            sl = np.s_[startday:]
         else:
-            sl = np.s_[:-pastdays]
+            sl = np.s_[startday:-pastdays]
             
         new_cases = df[new_cases_col].to_numpy()[sl]
         
@@ -94,13 +94,13 @@ def compute_past_series(df, new_cases_col, pastdays_start, pastdays_end, draws, 
         combined_trace = {'r_t': sampled_Rt}
 
         save_MCMC_sampling(
-            df, f'{new_cases_col}_{rt_col_prefix}', combined_trace, pastdays, interval=0.95, start=padding_left+1)
+            df, f'{new_cases_col}_{rt_col_prefix}', combined_trace, pastdays, interval=0.95, start=padding_left+1+startday)
 
         
         df.to_pickle(os.path.join(BASE_DATA_PATH,
             f'computed/WIP/{pickleprefix}_MCMC_Rt_pastdays_{pastdays_start:03d}_{pastdays_end:03d}.pickle'))    
 
-def main(pickleprefix, region, pastdays_start, pastdays_end, futuredraws, mctune, mcdraws):
+def main(pickleprefix, region, mctargetaccept, pastdays_start, pastdays_end, futuredraws, mctune, mcdraws, tot_chain_len):
 
     print(f'pastdays_start: {pastdays_start} - pastdays_end: {pastdays_end}')
 
@@ -119,12 +119,14 @@ def main(pickleprefix, region, pastdays_start, pastdays_end, futuredraws, mctune
     )
     regional_raw_data = regions_raw_data.loc[regions_raw_data.denominazione_regione==region].reset_index().copy()
 
+    STARTDAY=len(regional_raw_data.nuovi_positivi) - tot_chain_len
+
     compute_past_series(
-        regional_raw_data, 'nuovi_positivi', 
+        regional_raw_data, 'nuovi_positivi', STARTDAY,
         pickleprefix=f'{pickleprefix}_{region}',
         pastdays_start=pastdays_start, pastdays_end=pastdays_end, draws=futuredraws,
         alpha=alpha, beta=beta, trend_alpha=ALPHA, lower_ratio=0.8, upper_ratio=1.2,
-        mctune=mctune, mcdraws=mcdraws, mccores=8, mctargetaccept=0.99)
+        mctune=mctune, mcdraws=mcdraws, mccores=8, mctargetaccept=mctargetaccept)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2], *map(int, sys.argv[3:]))
+    main(sys.argv[1], sys.argv[2], float(sys.argv[3]), *map(int, sys.argv[4:]))
