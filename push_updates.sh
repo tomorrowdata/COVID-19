945305248fbf36f06c8ea53e56619b9349e720f5
Stefano Terna <stefano.terna@tomorrowdata.io>
2021-04-10 19:16:59 +0200

    push regions data

--- push_updates.sh
+++ push_updates.sh
@@ -1,11 +1,16 @@
 #UDATE=$(date --iso-8601)
-UDATE="YYYY-MM-DD"
+UDATE="2021-04-09"
 
 
 git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.csv
 git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.xlsx
 git add images/TD_Rt_computation_MCMC_300dpi.png
 git add images/TD_Rt_computation_MCMC_300dpi.jpg
+
+git add images/italy/TD_Rt_Regions_All_computation_MCMC_150dpi.jpg
+git add images/italy/TD_Rt_Regions_All_computation_MCMC_150dpi.png
+
+
 
 git add	images/italy/TD_Rt_Campania_computation_MCMC_150dpi.jpg
 git add	images/italy/TD_Rt_Campania_computation_MCMC_150dpi.png
@@ -20,9 +25,16 @@ git add notebooks/Rt_on_italian_national_data.ipynb
 git add notebooks/italy/Rt_Piemonte.ipynb
 git add notebooks/italy/Rt_regions.ipynb
 
+git add notebooks/italy/Rt-all-regions.ipynb
+git add notebooks/italy/Rt_Sicilia.ipynb
+git add notebooks/italy/Rt_Valle_d_Aosta.ipynb
+
+
+
 git add "data/computed/italy/${UDATE}_futbound_08_12_National_MCMC_Rt_pastdays_000_000.pickle"
 git add "data/computed/italy/${UDATE}_futbound_08_12_Piemonte_MCMC_Rt_pastdays_000_000.pickle"
 git add "data/computed/italy/${UDATE}_TD_calc_Regions_MCMC_Rt.pickle"
+git add "data/computed/italy/${UDATE}_TD_calc_Regions_all_MCMC_Rt.pickle"
 
 git commit -m "update $UDATE"
 
