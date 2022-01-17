9e429d02dbf88ac2c188e1e9877d91e4b9c246aa
Stefano Terna <stefano.terna@tomorrowdata.io>
2022-01-17 19:17:51 +0100

    check if calculations exist befor commiting; committing just picke files

--- automation/run_push_wip.sh
+++ automation/run_push_wip.sh
@@ -9,47 +9,47 @@ fi
 
 PASTDAYS="000"
 
+cd .. || exit 1
+
+source_dir="data/computed/WIP"
+target_dir="data/computed/italy"
+
+regions_file="${PLACEHOLDER_DATE}_TD_calc_Regions_all_MCMC_Rt.pickle"
+national_file="${PLACEHOLDER_DATE}_futbound_08_12_National_MCMC_Rt_pastdays_${PASTDAYS}_${PASTDAYS}.pickle"
+
+regions_file_source="${source_dir}/${regions_file}"
+national_file_source="${source_dir}/${national_file}"
+
+regions_file_target="${target_dir}/${regions_file}"
+national_file_target="${target_dir}/${national_file}"
+
+
+if [ ! -f $regions_file_source ]; then
+    echo "Regions file missing"
+    echo "Location should be $regions_file_source"
+    echo "Retry later"
+    exit 1
+fi
+
+if [ ! -f $national_file_source ]; then
+    echo "National file missing"
+    echo "Location should be $national_file_source"
+    echo "Retry later"    
+    exit 1
+fi
+
+sudo chown $UID:$UID $regions_file_source
+
+mv $regions_file_source $regions_file_target
+mv $national_file_source $national_file_target
+
 BRANCH_DATE=$(echo $PLACEHOLDER_DATE | sed -s "s#-##g")
 echo $BRANCH_DATE
 git checkout -b WIP_$BRANCH_DATE || exit 1
 
-cd .. || exit 1
+git add $regions_file_target || exit 1
+git add $national_file_target || exit 1
 
-
-git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.csv || exit 1
-git add data/computed/TD-covid19-ita-andamento-nazionale_Rt.xlsx || exit 1
-git add images/TD_Rt_computation_MCMC_300dpi.png || exit 1
-git add images/TD_Rt_computation_MCMC_300dpi.jpg || exit 1
-
-git add images/italy/TD_Rt_Regions_All_computation_MCMC_150dpi.jpg || exit 1
-git add images/italy/TD_Rt_Regions_All_computation_MCMC_150dpi.png || exit 1
-git add images/italy/TD_Rt_Regions_bars.jpg || exit 1
-git add images/italy/TD_Rt_Regions_bars.png || exit 1
-
-git add	images/italy/TD_Rt_Campania_computation_MCMC_150dpi.jpg || exit 1
-git add	images/italy/TD_Rt_Campania_computation_MCMC_150dpi.png || exit 1
-git add	images/italy/TD_Rt_Lombardia_computation_MCMC_150dpi.jpg || exit 1
-git add	images/italy/TD_Rt_Lombardia_computation_MCMC_150dpi.png || exit 1
-git add	images/italy/TD_Rt_Piemonte_computation_MCMC_150dpi.jpg || exit 1
-git add	images/italy/TD_Rt_Piemonte_computation_MCMC_150dpi.png || exit 1
-git add	images/italy/TD_Rt_Toscana_computation_MCMC_150dpi.jpg || exit 1
-git add	images/italy/TD_Rt_Toscana_computation_MCMC_150dpi.png || exit 1
-
-git add notebooks/Rt_on_italian_national_data.ipynb || exit 1
-git add notebooks/italy/Rt_Piemonte.ipynb || exit 1
-git add notebooks/italy/Rt_regions.ipynb || exit 1
-
-git add notebooks/italy/Rt-all-regions.ipynb || exit 1
-git add notebooks/italy/Rt_Sicilia.ipynb || exit 1
-git add notebooks/italy/Rt_Valle_d_Aosta.ipynb || exit 1
-
-
-git add "data/computed/italy/${PLACEHOLDER_DATE}_futbound_08_12_National_MCMC_Rt_pastdays_${PASTDAYS}_${PASTDAYS}.pickle" || exit 1
-git add "data/computed/italy/${PLACEHOLDER_DATE}_futbound_08_12_Piemonte_MCMC_Rt_pastdays_${PASTDAYS}_${PASTDAYS}.pickle" || exit 1
-git add "data/computed/italy/${PLACEHOLDER_DATE}_TD_calc_Regions_MCMC_Rt.pickle" || exit 1
-git add "data/computed/italy/${PLACEHOLDER_DATE}_TD_calc_Regions_all_MCMC_Rt.pickle" || exit 1
-git add "data/computed/italy/${PLACEHOLDER_DATE}_TD_calc_Regions_all_MCMC_Rt.csv" || exit 1
-
-git commit -m "update $PLACEHOLDER_DATE" || exit 1
+git commit -m "calculations $PLACEHOLDER_DATE" || exit 1
 
 git push --set-upstream origin WIP_$BRANCH_DATE
